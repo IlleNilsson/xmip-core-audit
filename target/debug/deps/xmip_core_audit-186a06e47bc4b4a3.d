56b4a9ff0c40e3b4c18ce0d79e47f94bd5c98ce1
Ilian Nilsson <ilian.nilsson@live.se>
2026-08-27 09:17:27 +0200

    Name the crate after its repository; take what this module owns

---
+++ target/debug/deps/xmip_core_audit-186a06e47bc4b4a3.d
@@ -0,0 +1,7 @@
+D:\Repos\Xmip\modules\operations\audit\target\debug\deps\xmip_core_audit-186a06e47bc4b4a3.d: src\lib.rs
+
+D:\Repos\Xmip\modules\operations\audit\target\debug\deps\libxmip_core_audit-186a06e47bc4b4a3.rlib: src\lib.rs
+
+D:\Repos\Xmip\modules\operations\audit\target\debug\deps\libxmip_core_audit-186a06e47bc4b4a3.rmeta: src\lib.rs
+
+src\lib.rs:
---
+++ target/debug/deps/xmip_core_audit-1fc654f57192b680.d
@@ -0,0 +1,5 @@
+D:\Repos\Xmip\modules\operations\audit\target\debug\deps\xmip_core_audit-1fc654f57192b680.d: src\lib.rs
+
+D:\Repos\Xmip\modules\operations\audit\target\debug\deps\xmip_core_audit-1fc654f57192b680.exe: src\lib.rs
+
+src\lib.rs:
